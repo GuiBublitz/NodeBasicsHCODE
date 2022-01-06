b1d19d45afe784d3c37e609b9a2e73b7b38bd71a
Guilherme Bublitz <gui.e.bublitz@gmail.com>
2022-01-06 14:45:43 -0200

    Excluindo um usuário

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,6 @@ let db = new NeDB({
 module.exports = (app)=>{
 
     let route = app.route('/users');
-
     route.get((req, res)=>{
         db.find({}).sort({name:1}).exec((err, users)=>{
             if(err){
@@ -30,7 +29,6 @@ module.exports = (app)=>{
             }
         });
     });
-    
     let routeId = app.route('/users/:id');
     routeId.get((req, res)=>{
         db.findOne({_id:req.params.id}).exec((err, user)=>{
@@ -41,6 +39,16 @@ module.exports = (app)=>{
             }
         });
     })
+    routeId.put((req, res)=>{
+        db.update({_id:req.params.id}, req.body, (err)=>{
+            if(err){
+                app.utils.error.send(err, req, res);
+            } else {
+                res.status(200).json(Object.assign(req.body, req.params));
+            }
+        });
+    });
+
 
     app.get('/user/admin',(req, res)=>{
         res.statusCode = 200;
